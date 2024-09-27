fc2e7c307807b782c9977c099740df3b964b592e
Gabriel Ford <gabe@fordltc.net>
2024-09-27 12:09:52 -0400

    Cargo format.

--- build.rs
+++ build.rs
@@ -9,5 +9,6 @@ fn main() {
 
     let mut file = File::create(dest.join("gl_bindings.rs")).unwrap();
     Registry::new(Api::Gles2, (3, 0), Profile::Core, Fallbacks::All, [])
-        .write_bindings(StructGenerator, &mut file).unwrap();
+        .write_bindings(StructGenerator, &mut file)
+        .unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,2 @@
-pub mod renderer;
 pub mod gl;
+pub mod renderer;
