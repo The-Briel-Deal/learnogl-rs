353d3fe3218cd7e54d151d8fe3a376906f26da53
Gabriel Ford <gabe@fordltc.net>
2024-09-27 00:28:31 -0400

    Got triangle drawing.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
 use core::panic;
-use std::{cell::RefCell, num::NonZeroU32, ops::Deref, rc::Rc};
+use std::{
+    borrow::Borrow,
+    cell::RefCell,
+    ffi::{CStr, CString},
+    num::NonZeroU32,
+    ops::Deref,
+    os::raw::c_void,
+    rc::Rc,
+};
 
+use gl::types::{GLfloat, GLuint};
 use glutin::{
     config::{Config, ConfigTemplateBuilder, GetGlConfig, GlConfig},
     context::{
         ContextApi, ContextAttributesBuilder, NotCurrentContext, PossiblyCurrentContext, Version,
     },
     display::GetGlDisplay,
-    prelude::{GlDisplay, NotCurrentGlContext},
+    prelude::{GlDisplay, NotCurrentGlContext, PossiblyCurrentGlContext},
+    surface::{GlSurface, Surface, SwapInterval, WindowSurface},
 };
-use glutin_winit::DisplayBuilder;
-use softbuffer::Surface;
+use glutin_winit::{DisplayBuilder, GlWindow};
 use winit::{
     application::ApplicationHandler,
     event::WindowEvent,
@@ -19,13 +28,160 @@ use winit::{
     window::{self, Window},
 };
 
-type RcSurf = Rc<RefCell<Surface<Rc<Window>, Rc<Window>>>>;
+pub mod gl {
+    #![allow(clippy::all)]
+    include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs"));
+
+    pub use Gles2 as Gl;
+}
+
+type RcSurf = Rc<RefCell<Surface<WindowSurface>>>;
+
+struct Renderer {
+    program: gl::types::GLuint,
+    vao: gl::types::GLuint,
+    vbo: gl::types::GLuint,
+    gl: gl::Gl,
+}
+
+impl Renderer {
+    pub fn new<D: GlDisplay>(gl_display: &D) -> Self {
+        let gl = gl::Gl::load_with(|symbol| {
+            let symbol = CString::new(symbol).unwrap();
+            gl_display.get_proc_address(symbol.as_c_str()).cast()
+        });
+
+        if let Some(renderer) = get_gl_string(&gl, gl::RENDERER) {
+            println!("Running on {}", renderer.to_string_lossy());
+        }
+        if let Some(version) = get_gl_string(&gl, gl::VERSION) {
+            println!("OpenGL Version {}", version.to_string_lossy());
+        }
+
+        if let Some(shaders_version) = get_gl_string(&gl, gl::SHADING_LANGUAGE_VERSION) {
+            println!("Shaders version on {}", shaders_version.to_string_lossy());
+        }
+        unsafe {
+            let vertex_shader = create_shader(&gl, gl::VERTEX_SHADER, VERTEX_SHADER_SOURCE);
+
+            let fragment_shader = create_shader(&gl, gl::FRAGMENT_SHADER, FRAGMENT_SHADER_SOURCE);
+
+            let program = gl.CreateProgram();
+
+            gl.AttachShader(program, vertex_shader);
+            gl.AttachShader(program, fragment_shader);
+
+            gl.LinkProgram(program);
+
+            gl.UseProgram(program);
+
+            gl.DeleteShader(vertex_shader);
+            gl.DeleteShader(fragment_shader);
+
+            let mut vao = std::mem::zeroed();
+            gl.GenVertexArrays(1, &mut vao);
+            gl.BindVertexArray(vao);
+
+            let mut vbo = std::mem::zeroed();
+            gl.GenBuffers(1, &mut vbo);
+            gl.BindBuffer(gl::ARRAY_BUFFER, vbo);
+            gl.BufferData(
+                gl::ARRAY_BUFFER,
+                (VERTEX_DATA.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr,
+                VERTEX_DATA.as_ptr() as *const _,
+                gl::STATIC_DRAW,
+            );
+
+            let pos_attrib =
+                gl.GetAttribLocation(program, b"position\0".as_ptr() as *const gl::types::GLchar);
+            let color_attrib =
+                gl.GetAttribLocation(program, b"color\0".as_ptr() as *const gl::types::GLchar);
+            dbg!(pos_attrib, color_attrib);
+
+            gl.VertexAttribPointer(
+                pos_attrib as gl::types::GLuint,
+                2,
+                gl::FLOAT,
+                gl::FALSE,
+                5 * std::mem::size_of::<f32>() as gl::types::GLsizei,
+                std::ptr::null(),
+            );
+            gl.VertexAttribPointer(
+                color_attrib as gl::types::GLuint,
+                3,
+                gl::FLOAT,
+                0,
+                5 * std::mem::size_of::<f32>() as gl::types::GLsizei,
+                (2 * std::mem::size_of::<f32>()) as *const c_void,
+            );
+            gl.EnableVertexAttribArray(pos_attrib as GLuint);
+            gl.EnableVertexAttribArray(color_attrib as GLuint);
+
+            Self {
+                program,
+                vao,
+                vbo,
+                gl,
+            }
+        }
+    }
+    pub fn draw(&self) {
+        self.draw_with_clear_color(0.1, 0.1, 0.1, 0.9)
+    }
+
+    pub fn draw_with_clear_color(
+        &self,
+        red: GLfloat,
+        green: GLfloat,
+        blue: GLfloat,
+        alpha: GLfloat,
+    ) {
+        unsafe {
+            self.gl.UseProgram(self.program);
+
+            self.gl.BindVertexArray(self.vao);
+            self.gl.BindBuffer(gl::ARRAY_BUFFER, self.vbo);
+
+            self.gl.ClearColor(red, green, blue, alpha);
+            self.gl.Clear(gl::COLOR_BUFFER_BIT);
+            self.gl.DrawArrays(gl::TRIANGLES, 0, 3);
+        }
+    }
+    pub fn resize(&self, width: i32, height: i32) {
+        unsafe { self.gl.Viewport(0, 0, width, height) }
+    }
+}
+
+unsafe fn create_shader(
+    gl: &gl::Gl,
+    shader: gl::types::GLenum,
+    source: &[u8],
+) -> gl::types::GLuint {
+    let shader = gl.CreateShader(shader);
+    gl.ShaderSource(
+        shader,
+        1,
+        [source.as_ptr().cast()].as_ptr(),
+        std::ptr::null(),
+    );
+    gl.CompileShader(shader);
+    shader
+}
+
+fn get_gl_string(gl: &gl::Gl, variant: gl::types::GLenum) -> Option<&'static CStr> {
+    unsafe {
+        let s = gl.GetString(variant);
+        (!s.is_null()).then(|| CStr::from_ptr(s.cast()))
+    }
+}
+
 struct App {
     window: Option<Rc<Window>>,
     surface: Option<RcSurf>,
     template: ConfigTemplateBuilder,
     gl_display: GlDisplayCreationState,
     gl_context: Option<PossiblyCurrentContext>,
+    renderer: Option<Renderer>,
 }
 
 impl ApplicationHandler for App {
@@ -38,7 +194,6 @@ impl ApplicationHandler for App {
                     |configs| {
                         configs
                             .reduce(|accum, config| {
-                                dbg!(&config);
                                 if config.num_samples() > accum.num_samples() {
                                     config
                                 } else {
@@ -74,15 +229,32 @@ impl ApplicationHandler for App {
                 }
             }
         };
-        // self.window = Some(Rc::new(
-        //     event_loop
-        //         .create_window(Window::default_attributes())
-        //         .unwrap(),
-        // ));
-        // let context = softbuffer::Context::new(self.window.clone().unwrap()).unwrap();
-        // self.surface = Some(Rc::new(RefCell::new(
-        //     softbuffer::Surface::new(&context, self.window.clone().unwrap()).unwrap(),
-        // )));
+
+        let attrs = window
+            .build_surface_attributes(Default::default())
+            .expect("Failed to build surface attributes");
+        let gl_surface = unsafe {
+            gl_config
+                .display()
+                .create_window_surface(&gl_config, &attrs)
+                .unwrap()
+        };
+
+        let gl_context = self.gl_context.as_ref().unwrap();
+
+        gl_context.make_current(&gl_surface).unwrap();
+
+        self.renderer
+            .get_or_insert_with(|| Renderer::new(&gl_config.display()));
+
+        // Try setting vsync.
+        if let Err(res) = gl_surface
+            .set_swap_interval(gl_context, SwapInterval::Wait(NonZeroU32::new(1).unwrap()))
+        {
+            eprintln!("Error setting vsync: {res:?}");
+        }
+        self.surface = Some(Rc::new(RefCell::new(gl_surface)));
+        self.window = Some(Rc::new(window));
     }
 
     fn window_event(
@@ -98,29 +270,38 @@ impl ApplicationHandler for App {
                 event_loop.exit();
             }
             WindowEvent::RedrawRequested => {
-                let surface = self.surface.clone().unwrap();
-                let mut surface = surface.borrow_mut();
+                let window = self.window.as_ref().unwrap();
+                let gl_surface = self.surface.as_ref().unwrap();
+                let gl_surface = gl_surface.deref().borrow();
 
-                let buffer = surface.buffer_mut();
-                let mut buffer = buffer.unwrap();
+                let gl_context = self.gl_context.as_ref().unwrap();
+                let renderer = self.renderer.as_ref().unwrap();
+                renderer.draw();
+                window.request_redraw();
 
-                for pixel in buffer.iter_mut() {
-                    *pixel = u32::MIN;
-                }
-                let _ = buffer.present();
-                // self.window.as_ref().unwrap().request_redraw();
+                gl_surface.swap_buffers(gl_context).unwrap();
             }
             WindowEvent::Resized(size) => {
-                if let (Some(width), Some(height)) =
-                    (NonZeroU32::new(size.width), NonZeroU32::new(size.height))
-                {
-                    self.surface
-                        .clone()
-                        .unwrap()
-                        .borrow_mut()
-                        .resize(width, height)
-                        .unwrap();
+                if let Some(gl_surface) = self.surface.clone() {
+                    let gl_context = self.gl_context.as_ref().unwrap();
+
+                    let gl_surface = gl_surface.deref().borrow();
+                    gl_surface.resize(
+                        gl_context,
+                        NonZeroU32::new(size.width).unwrap(),
+                        NonZeroU32::new(size.height).unwrap(),
+                    );
+
+                    let renderer = self.renderer.as_ref().unwrap();
+                    renderer.resize(size.width as i32, size.height as i32);
                 }
+                // if let (Some(width), Some(height), Some(surface)) = (
+                //     NonZeroU32::new(size.width),
+                //     NonZeroU32::new(size.height),
+                //     self.surface.clone(),
+                // ) {
+                //     surface.borrow_mut().resize(width, height).unwrap();
+                // }
             }
             _ => (),
         }
@@ -153,6 +334,7 @@ fn main() {
         window: None,
         surface: None,
         gl_context: None,
+        renderer: None,
     };
     let _ = event_loop.run_app(&mut app);
 }
@@ -194,3 +376,36 @@ fn create_gl_context(window: &Window, gl_config: &Config) -> NotCurrentContext {
             })
     }
 }
+
+#[rustfmt::skip]
+static VERTEX_DATA: [f32; 15] = [
+    -0.5, -0.5,  1.0,  0.0,  0.0,
+     0.0,  0.5,  0.0,  1.0,  0.0,
+     0.5, -0.5,  0.0,  0.0,  1.0,
+];
+
+const VERTEX_SHADER_SOURCE: &[u8] = b"
+#version 100
+precision mediump float;
+
+attribute vec2 position;
+attribute vec3 color;
+
+varying vec3 v_color;
+
+void main() {
+    gl_Position = vec4(position, 0.0, 1.0);
+    v_color = color;
+}
+\0";
+
+const FRAGMENT_SHADER_SOURCE: &[u8] = b"
+#version 100
+precision mediump float;
+
+varying vec3 v_color;
+
+void main() {
+    gl_FragColor = vec4(v_color, 1.0);
+}
+\0";
