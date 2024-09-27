08e71608a6ecb9917333c10f041cf8d160a795c8
Gabriel Ford <gabe@fordltc.net>
2024-09-27 12:51:56 -0400

    Move context creation to gl module

--- src/gl.rs
+++ src/gl.rs
@@ -3,7 +3,14 @@ include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs"));
 
 use std::ffi::CStr;
 
+use glutin::{
+    config::Config,
+    context::{ContextApi, ContextAttributesBuilder, NotCurrentContext, Version},
+    display::GetGlDisplay,
+    prelude::GlDisplay,
+};
 use types::{GLenum, GLuint};
+use winit::{raw_window_handle::HasWindowHandle, window::Window};
 pub use Gles2 as Gl;
 
 pub fn get_gl_string(gl: &Gl, variant: GLenum) -> Option<&'static CStr> {
@@ -23,4 +30,34 @@ pub unsafe fn create_shader(gl: &Gl, shader: GLenum, source: &[u8]) -> GLuint {
     );
     gl.CompileShader(shader);
     shader
+}
+
+pub fn create_gl_context(window: &Window, gl_config: &Config) -> NotCurrentContext {
+    let raw_window_handle = window.window_handle().ok().map(|wh| wh.as_raw());
+
+    let context_attributes = ContextAttributesBuilder::new().build(raw_window_handle);
+
+    let fallback_context_attributes = ContextAttributesBuilder::new()
+        .with_context_api(ContextApi::Gles(None))
+        .build(raw_window_handle);
+
+    let legacy_context_attributes = ContextAttributesBuilder::new()
+        .with_context_api(ContextApi::OpenGl(Some(Version::new(2, 1))))
+        .build(raw_window_handle);
+
+    let gl_display = gl_config.display();
+
+    unsafe {
+        gl_display
+            .create_context(gl_config, &context_attributes)
+            .unwrap_or_else(|_| {
+                gl_display
+                    .create_context(gl_config, &fallback_context_attributes)
+                    .unwrap_or_else(|_| {
+                        gl_display
+                            .create_context(gl_config, &legacy_context_attributes)
+                            .expect("failed to create context")
+                    })
+            })
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ use winit::{
     raw_window_handle::HasWindowHandle,
     window::Window,
 };
-use winit_test::renderer::Renderer;
+use winit_test::{gl::create_gl_context, renderer::Renderer};
 
 struct App {
     window: Option<Rc<Window>>,
@@ -172,34 +172,4 @@ fn main() {
         renderer: None,
     };
     let _ = event_loop.run_app(&mut app);
-}
-
-fn create_gl_context(window: &Window, gl_config: &Config) -> NotCurrentContext {
-    let raw_window_handle = window.window_handle().ok().map(|wh| wh.as_raw());
-
-    let context_attributes = ContextAttributesBuilder::new().build(raw_window_handle);
-
-    let fallback_context_attributes = ContextAttributesBuilder::new()
-        .with_context_api(ContextApi::Gles(None))
-        .build(raw_window_handle);
-
-    let legacy_context_attributes = ContextAttributesBuilder::new()
-        .with_context_api(ContextApi::OpenGl(Some(Version::new(2, 1))))
-        .build(raw_window_handle);
-
-    let gl_display = gl_config.display();
-
-    unsafe {
-        gl_display
-            .create_context(gl_config, &context_attributes)
-            .unwrap_or_else(|_| {
-                gl_display
-                    .create_context(gl_config, &fallback_context_attributes)
-                    .unwrap_or_else(|_| {
-                        gl_display
-                            .create_context(gl_config, &legacy_context_attributes)
-                            .expect("failed to create context")
-                    })
-            })
-    }
 }
