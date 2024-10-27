dab4bc9d46a805bc537255a482d90d00762a6139
Gabriel Ford <gabe@fordltc.net>
2024-10-26 23:34:23 -0400

    Restructure to closer match the example shader.

--- src/shader/light_casters_frag.glsl
+++ src/shader/light_casters_frag.glsl
@@ -32,19 +32,12 @@ uniform Light light;
 
 void main()
 {
-    vec3 lightDir = normalize(light.position - FragPos);
-
-    // check if lighting is inside the spotlight cone
-    float theta = dot(lightDir, normalize(-light.direction));
-
-    float epsilon = light.innerCutOff - light.outerCutOff;
-    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 20.0);
-
     // ambient
     vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
 
     // diffuse
     vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;
 
@@ -54,16 +47,21 @@ void main()
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
     vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;
 
+    // spotlight (soft edges)
+    float theta = dot(lightDir, normalize(-light.direction));
+    float epsilon = (light.innerCutOff - light.outerCutOff);
+    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
+    diffuse *= intensity;
+    specular *= intensity;
+
     // attenuation
     float distance = length(light.position - FragPos);
     float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
-
-    // ambient  *= attenuation; // remove attenuation from ambient, as otherwise at large distances the light would be darker inside than outside the spotlight due the ambient term in the else branch
+    ambient *= attenuation;
     diffuse *= attenuation;
     specular *= attenuation;
-    diffuse *= intensity;
-    specular *= intensity;
 
     vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
 }
+
