85698e9cfc07f747f0e64a5d96b625a0c865b434
Gabriel Ford <gabe@fordltc.net>
2024-11-02 00:13:30 -0400

    Added a comment to the assimp module

--- src/assimp.rs
+++ src/assimp.rs
@@ -1,4 +1,5 @@
 #![allow(clippy::all)]
+// I had to do this because of u128s, apparently its not really a problem anymore though?
 #![allow(improper_ctypes)]
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
